12c9720cb3c320fd9e541433c8e483c336609832
TyIsI <TyIsI@users.noreply.github.com>
2024-09-29 12:03:32 -0700

    stories(frontend): 📖 formatting of various stories files (#7)

--- packages/frontend/src/components/App/App.lazy.tsx
+++ packages/frontend/src/components/App/App.lazy.tsx
@@ -2,9 +2,7 @@ import React, { lazy, Suspense } from 'react'
 
 const LazyApp = lazy(() => import('./App'))
 
-const App = (
-    props: JSX.IntrinsicAttributes & { children?: React.ReactNode }
-) => (
+const App = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode }) => (
     <Suspense fallback={null}>
         <LazyApp {...props} />
     </Suspense>
