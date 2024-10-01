4006c115ba5568207e6eba5e30ee5c0da7ef88d6
Carson James <Carsonjames@shaw.ca>
2024-10-01 01:20:57 -0700

    feat(frontend): ✨ initial frontend sidebar
    initial design for a animated nav/sidebar

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     darkMode: ['class', '[data-theme="dark"]'],
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                'header-cutive': ['Cutive']
+            },
+            colors: {
+                'header-color': '#004E63',
+                'header-text': '#F7F6F6',
+                'page-color': '#1f1f21',
+
+                gray: {
+                    900: '#202225',
+                    800: '#2f3136',
+                    700: '#36393f',
+                    600: '#4f545c',
+                    400: '#d4d7dc',
+                    300: '#e3e5e8',
+                    200: '#ebedef',
+                    100: '#f2f3f5'
+                }
+            }
+        }
     },
     plugins: []
 }
