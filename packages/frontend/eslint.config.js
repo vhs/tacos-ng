0fea30791a79629005c168b556d0d1a326d967d7
Carson James <Carsonjames@shaw.ca>
2024-10-01 00:42:18 -0700

    package(frontend): 📦 changed eslint rule
    changed error on unused var for eslint

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -18,6 +18,7 @@ export default tseslint.config(
             'react-refresh': reactRefresh
         },
         rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
         }
