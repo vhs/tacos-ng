912bfda814fdae6dae75c25c5ee6b0c9aa5f7122
Carson James <Carsonjames@shaw.ca>
2024-10-01 17:17:15 -0700

    formatting(frontend): 📝 updated css class names
    updated css class names for the sidebar

--- packages/frontend/src/components/SideBarVertical/SideBarVertical.tsx
+++ packages/frontend/src/components/SideBarVertical/SideBarVertical.tsx
@@ -15,8 +15,8 @@ const SideBarIcons = [
 export function SideBarVertical() {
     const [isExpanded, setIsExpanded] = useState(true);
     return (
-        <div className={`sidebar ${!isExpanded && 'sidebar-collapsed'}`}>
-            <div className='sidebar-header'>TACOS</div>
+        <div className={`sidebar-v ${!isExpanded && 'sidebar-v-collapsed'}`}>
+            <div className='sidebar-v-header'>TACOS</div>
             <div className='mt-3'>
                 {SideBarIcons.map((i) => (
                     <SideBarIcon icon={i.icon} text={i.text} />
@@ -26,7 +26,7 @@ export function SideBarVertical() {
                 onClick={() => {
                     setIsExpanded(!isExpanded)
                 }}
-                className='sidebar-expand-button'
+                className='sidebar-v-expand-button'
             >
                 {isExpanded ? <FaAngleLeft size='20'></FaAngleLeft> : <FaAngleRight size='20'></FaAngleRight>}
             </div>
