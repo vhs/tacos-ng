1b369511b7017d1a1fe43347bde136a85f8b8648
Carson James <Carsonjames@shaw.ca>
2024-10-01 17:17:15 -0700

    formatting(frontend): 📝 updated css class names
    updated css class names for the sidebar

--- packages/frontend/src/components/SideBarIcon/SideBarIcon.tsx
+++ packages/frontend/src/components/SideBarIcon/SideBarIcon.tsx
@@ -7,10 +7,10 @@ interface SideBarIconProps {
 
 const SideBarIcon: FC<SideBarIconProps> = (props) => (
     <div className='flex items-center'>
-        <div className='sidebar-icon'>
+        <div className='sidebar-v-icon'>
             {props.icon}
-            <span className='sidebar-icon-text'>{props.text}</span>
-            <span className='sidebar-tooltip'>{props.text}</span>
+            <span className='sidebar-v-icon-text'>{props.text}</span>
+            <span className='sidebar-v-tooltip'>{props.text}</span>
         </div>
     </div>
 )
