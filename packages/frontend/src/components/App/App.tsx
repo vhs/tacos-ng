c27edff4de066f5498d254e4f5fbdd5b0ab1ab18
Carson James <Carsonjames@shaw.ca>
2024-10-04 17:19:14 -0700

    feat(frontend): ✨ add media-width checking
    add media-width checking to control mounting of MobileBar vs DeskTopBar

--- packages/frontend/src/components/App/App.tsx
+++ packages/frontend/src/components/App/App.tsx
@@ -1,12 +1,25 @@
+import MobileBar from '../MobileBar/MobileBar'
 import Devices from '../Devices/Devices'
 import SideBarVertical from '../SideBarVertical/SideBarVertical'
 import './App.css'
+import { useState, useEffect } from 'react'
 
 export function App() {
+    const [isMobile, setIsMobile] = useState<boolean>(false)
+
+    const handleReize = () => {
+        setIsMobile(window.matchMedia('(max-width: 640px)').matches)
+    }
+
+    useEffect(() => {
+        handleReize()
+        window.addEventListener('resize', handleReize)
+        return () => window.removeEventListener('resize', handleReize)
+    }, [])
 
     return (
         <div className='flex h-screen'>
-            <SideBarVertical />
+            {isMobile ? <MobileBar /> : <SideBarVertical />}
             <Devices />
         </div>
     )
