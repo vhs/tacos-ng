1d4d7e8d8004fec6bae038e81e1a40d5e1029321
Carson James <carsonjames@shaw.ca>
2024-11-20 17:23:24 -0800

    feat(frontend): ✨ started work on terminals page
    added folder scaffolding and basic boilerplate

--- packages/frontend/src/components/Pages/TerminalsPage/TerminalsPage.tsx
+++ packages/frontend/src/components/Pages/TerminalsPage/TerminalsPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import Subheader from '../../Header/Subheader'
+import Devices from './Terminals/Terminals'
 
 const TerminalsPage: FC = () => {
     return (
         <div>
             <Subheader text='Terminals' />
+            <Devices />
         </div>
     )
 }
