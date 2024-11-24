67aa474fbbb246c270a3f8f48e1bb2365b692026
Carson James <Carsonjames@shaw.ca>
2024-11-24 13:57:20 -0800

    dev(frontend): 🧰 disabled ESLINT no explict any rule
    working with intended anys for general use

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -19,6 +19,7 @@ export default tseslint.config(
         },
         rules: {
             '@typescript-eslint/no-unused-vars': 'off',
+            "@typescript-eslint/no-explicit-any": "off",
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
         }
